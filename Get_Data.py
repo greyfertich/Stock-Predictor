11c41b783b908a12987c078013d12139909373dc
Grey Fertich <greyfertich@gmail.com>
2018-05-07 19:00:48 -0400

    Update Get_Data.py
    get_price method now uses quandl API

--- Get_Data.py
+++ Get_Data.py
@@ -2,9 +2,16 @@ import pandas_datareader.data as web
 
 class GetData:
     def get_price(symbol, start, end):
-        df = web.DataReader(symbol, 'iex', start, end)
-        df = df.drop('open',1)
-        df = df.drop('high',1)
-        df = df.drop('low',1)
-        df = df.drop('volume',1)
+        df = web.DataReader(symbol, 'quandl', start, end)
+        df = df.drop('Open',1)
+        df = df.drop('High',1)
+        df = df.drop('Low',1)
+        df = df.drop('Volume',1)
+        df = df.drop('ExDividend',1)
+        df = df.drop('SplitRatio',1)
+        df = df.drop('AdjOpen',1)
+        df = df.drop('AdjHigh',1)
+        df = df.drop('AdjLow',1)
+        df = df.drop('AdjClose',1)
+        df = df.drop('AdjVolume',1)
         return df
